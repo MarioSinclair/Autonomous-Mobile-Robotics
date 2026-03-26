7e0e0bb032410d91ed0b2264bd5eb4ea213deba8
Mario Sinclair <mariosinclair@Marios-MacBook-Air.local>
2026-03-26 12:18:00 -0400

    new

--- my_robot.py
+++ my_robot.py
@@ -270,7 +270,7 @@ if __name__ == "__main__":
 
         print("Wall fallowing...")
         robot.wall_follow(direction='left', setpoint=300, max_velocity=40,
-                            Kp_dist=10, Kp_angle=10, Kp_fwd=10,
+                            Kp_dist=1, Kp_angle=1, Kp_fwd=1,
                             stop_distance=500
                           )
 
