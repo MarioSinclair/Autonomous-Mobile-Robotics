80c004dc66a157351c631095e1f0552aada27d2f
Mario Sinclair <mariosinclair@Marios-MacBook-Air.local>
2026-03-26 11:56:48 -0400

    new

--- my_robot.py
+++ my_robot.py
@@ -105,7 +105,7 @@ class MyRobot(HamBot):
         
         self.stop_motors()
         
-    def lidar_move_forward(self, stop_distance=108, max_velocity=50, Kp=20.0, tolerance=0.1):
+    def lidar_move_forward(self, stop_distance=110, max_velocity=50, Kp=20.0, tolerance=10):
         while True:
             lidar_data = self.get_lidar_range_image()
             front_distance = lidar_data[180]
